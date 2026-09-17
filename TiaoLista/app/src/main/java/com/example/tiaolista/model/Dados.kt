1a9b0472107f74ecdf59d744397cfefd385e9a25
Manu-011 <emanuelcgalindo@hotmail.com>
2026-09-17 20:11:11 -0300

    add Oficina mais um

---
+++ TiaoLista/app/src/main/java/com/example/tiaolista/model/Dados.kt
@@ -0,0 +1,29 @@
+package com.example.tiaolista.model
+
+val listaTiposCombustivel = listOf(
+
+    TipoCombustivel(
+        1,
+        "Álcool / Etanol"
+    ),
+    TipoCombustivel(
+        2,
+        "Gasolina"
+    ),
+    TipoCombustivel(
+        3,
+        "Gás"
+    ),
+    TipoCombustivel(
+        4,
+        "Flex"
+    ),
+    TipoCombustivel(
+        5,
+        "Híbrido"
+    ),
+    TipoCombustivel(
+        6,
+        "Elétrico"
+    )
+)
---
+++ TiaoLista/app/src/main/java/com/example/tiaolista/model/TIpoCombustivel.kt
@@ -0,0 +1,6 @@
+package com.example.tiaolista.model
+
+data class TipoCombustivel(
+    val idTipoCombustivel: Int,
+    val tipoCombustivel: String
+)
